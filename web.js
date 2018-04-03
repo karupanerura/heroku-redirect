85ee3aee4169b119932de51bcf9ade761cc26e95
karupanerura <karupa@cpan.org>
2018-04-03 09:25:47 +0900

    fix

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var app = express();
+"use strict";
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
-var redirectStatusCode = process.env.REDIRECT_STATUS_CODE || 302;
-var port = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+
+const newBaseURL         = process.env.NEW_BASE_URL         || 'http://example.com';
+const redirectStatusCode = process.env.REDIRECT_STATUS_CODE || 302;
+const port               = process.env.PORT                 || 5000;
 
 app.get('*', function(request, response) {
   response.redirect(redirectStatusCode, newBaseURL + request.url);
 });
 
 app.listen(port, function() {
-  console.log("Redirect to "+newBaseURL+" status:"+redirectStatusCode);
-  console.log("Listening on " + port);
+  console.log(`Redirect to ${newBaseURL} (status:${redirectStatusCode})`);
+  console.log(`Listening on ${port}`);
 });
