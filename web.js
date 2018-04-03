3d812c3894faf2a38216d489f9445a390be0c331
karupanerura <karupa@cpan.org>
2018-04-03 09:09:03 +0900

    set status code

--- web.js
+++ web.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 
 var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var redirectStatusCode = process.env.REDIRECT_STATUS_CODE || 302;
 var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
-  response.redirect(newBaseURL + request.url);
+  response.redirect(redirectStatusCode, newBaseURL + request.url);
 });
 
 app.listen(port, function() {
