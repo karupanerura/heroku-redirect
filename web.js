27a5e142980468469d6e828cc5fb02d7267f924b
karupanerura <karupa@cpan.org>
2018-04-03 09:40:12 +0900

    fix

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const express = require('express');
-const app = express();
 
 const newBaseURL         = process.env.NEW_BASE_URL         || 'http://example.com';
 const redirectStatusCode = process.env.REDIRECT_STATUS_CODE || 302;
 const port               = process.env.PORT                 || 5000;
+
+const app = express();
 
 app.get('*', function(request, response) {
   response.redirect(redirectStatusCode, newBaseURL + request.url);
